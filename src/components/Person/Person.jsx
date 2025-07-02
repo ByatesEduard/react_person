73ab2a5fdab419310c6b05c0ccb581e39b94fa7b
EduardByatec <edikbatec1@gmail.com>
2025-07-02 23:20:55 +0300

    add

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <div className="Person">
+      <p className="Person__name">My name is {name}</p>
+
+      {age !== undefined && (
+        <p className="Person__age">I am {age} years old</p>
+      )}
+
+      {isMarried ? (
+        <p className="Person__partner">
+          My {sex === 'm' ? 'wife' : 'husband'} is {partnerName}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </div>
+  );
+}
